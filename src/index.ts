0cb0f37527c3e8c2b2d7afdf9d5a7d6ee0f52170
Guy Hed <guyhe@cet.ac.il>
2022-12-04 00:30:30 +0200

    write to file

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import { webCrawler } from './webCrawler';
+import fs from 'fs';
 
 
 
-const program = async (url:string, depth: number) => {
+
+const program = async (url: string, depth: number) => {
     const gotModule = await import('got');
-    const gotBody = (url: string) => gotModule.got (url).then(r => r.body);
+    const gotBody = (url: string) => gotModule.got(url).then(r => r.body);
     const result = await webCrawler(gotBody, url, depth)
-    console.log(result);
+    await new Promise(resolve => fs.writeFile("results.json", JSON.stringify(result), resolve));
 }
 
 
 const [url, sDepth] = process.argv.slice(2);
-program(url,parseInt(sDepth));
+program(url, parseInt(sDepth));
 
--- src/webCrawler.ts
+++ src/webCrawler.ts
@@ -17,21 +17,29 @@ const isWebUrl = (uri: string) => {
     }
 }
 
-const fixProtocol = (url: string) => url?.replace(/^\/\//, "https://");
+const fixProtocol = (url: string, sourceUrl: string) => {
+    if (url.startsWith("//")) return "https:" + url;
+    else if (url.startsWith("/")) return (new URL(sourceUrl)).origin + url;
+    else return url;
+}
 
 export const webCrawler = async (gotBody: (url: string) => Promise<string>, url: string, depth: number) => {
-    const body = await gotBody(url);
-    const dom = new JSDOM(body);
-    const domElements = Array.from(dom.window.document.querySelectorAll('*'));
-    const imageList = domElements.map(getImageUrl).filter(u => !!u)
-        .map(imageUrl => ({ depth: 0, sourceUrl: url, imageUrl } as CrawlResult));
-    const links = domElements.filter(isTag("a"))
-        .map(a => fixProtocol((a as HTMLAnchorElement).href)).filter(isWebUrl);
-    if (depth > 0) {
-        const subLists = await Promise.all(links.map(l => webCrawler(gotBody, l, depth - 1)));
-        const joined = ([] as CrawlResult[]).concat(...subLists);
-        joined.forEach(i => i.depth++);
-        imageList.push(...joined);
-    } 
-    return imageList;
+    try {
+        const body = await gotBody(url);
+        const dom = new JSDOM(body);
+        const domElements = Array.from(dom.window.document.querySelectorAll('*'));
+        const imageList = domElements.map(getImageUrl).filter(u => !!u)
+            .map(imageUrl => ({ depth: 0, sourceUrl: url, imageUrl } as CrawlResult));
+        const links = domElements.filter(isTag("a"))
+            .map(a => fixProtocol((a as HTMLAnchorElement).href, url)).filter(isWebUrl);
+        if (depth > 0) {
+            const subLists = await Promise.all(links.map(l => webCrawler(gotBody, l, depth - 1)));
+            const joined = ([] as CrawlResult[]).concat(...subLists);
+            joined.forEach(i => i.depth++);
+            imageList.push(...joined);
+        }
+        return imageList;
+    } catch {
+        return [] as CrawlResult[];
+    }
 }
